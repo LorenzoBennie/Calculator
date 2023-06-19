b586b700267a16c0e6696920ab966ed0c75f1fec
LorenzoBennie <131253527+LorenzoBennie@users.noreply.github.com>
2023-06-19 18:55:17 +0200

    Add a function that runs one operation on the calculator

--- javascript.js
+++ javascript.js
@@ -13,3 +13,22 @@ function multiplication (a,b) {
 function division (a,b) {
   return a / b;
 }
+
+let firstNumber;
+let secondumber;
+let operator;
+
+function operate (a,b) {
+  if (operator === "+") {
+    addition (a,b);
+  }
+  else if (operator === "-") {
+    subtraction (a,b);
+  }
+  else if (operator === "*") {
+    multiplication(a,b);
+  }
+  else if (operator === "/") {
+    division (a,b);
+  }
+}
